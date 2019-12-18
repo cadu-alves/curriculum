adcfd13c6c715254b7a08da14176d13a9738da27
zarbeon <56039416+zarbeon@users.noreply.github.com>
2019-12-18 00:13:39 -0300

    innitial commit

---
+++ content/scripts/tooltips.js
@@ -0,0 +1,54 @@
+// tippy ('', {
+//     theme: 'cadore',
+//     placement: 'right',
+//     maxWidth: '250px',
+//     content: ''
+// })
+
+// -------------------- Competencias:
+tippy ('.ttpIngles', {
+    theme: 'cadore',
+    placement: 'left',
+    maxWidth: '250px',
+    content: '<strong> Leitura:</strong> Avançado <br><br> <strong> Escrita:</strong> Avaçado <br><br> <strong> Conversação:</strong> Mediano',
+});
+tippy ('.ttpHtml', {
+    theme: 'cadore',
+    placement: 'left',
+    maxWidth: '250px',
+    content: 'De todas as pessoas que conheço, eu sou o melhor nessas linguagens. <br><br> Falo com tranquilidade.'
+})
+tippy ('.ttpSass', {
+    theme: 'cadore',
+    placement: 'left',
+    maxWidth: '250px',
+    content: 'Nada muito complexo, mas eu sei me virar sozinho na maioria dos dilemas cotidianos.'
+})
+tippy ('.ttpPintura', {
+    theme: 'cadore',
+    placement: 'left',
+    maxWidth: '250px',
+    content: 'Um hobbie da infância que carrego até os dias atuais.<br><br> Já cheguei inclusive a trabalhar com encomendas de personagens antropomorfos.'
+})
+tippy ('.ttpAdobe', {
+    theme: 'cadore',
+    placement: 'left',
+    maxWidth: '250px',
+    content: 'Photoshop e Illustrator são as minhas principais ferramentas de criação.<br><br> Além do Adobe XD, também uso o Figma como plataforma de prototipagem.'
+})
+
+// -------------------- Experiencia:
+tippy ('.ttpScytl', {
+    theme: 'cadore',
+    placement: 'top',
+    maxWidth: '300px',
+    content: 'Empresa Multinacional especialista em soluções em voto eletrônico no mundo todo.<br> Fundada em Barcelona em 2001, chegou ao Brasil dez anos depois.'
+})
+tippy ('.ttpHepta', {
+    theme: 'cadore',
+    placement: 'top',
+    maxWidth: '300px',
+    content: 'A Hepta Informática é especialista no desenvolvimento e soluções em TI para o mercado corporativo.<br> Parceira oficial Microsoft no Brasil.'
+})
+
+// -------------------- Diferenciais:
